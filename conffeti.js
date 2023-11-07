74c2f976c358d3d6ea50074b496c5cc6dd977ed8
Magnus <134337162+Magnsz@users.noreply.github.com>
2023-11-07 15:33:18 -0300

    Update conffeti.js

--- conffeti.js
+++ conffeti.js
@@ -1,3 +1,11 @@
-const confettiSettings = { target: 'confetti-container' };
-const confetti = new ConfettiGenerator(confettiSettings);
-confetti.render();
+const confettiContainer = document.querySelector('.confetti-container');
+const colors = ['#ffcc33', '#ff3366', '#33cc33', '#3399ff', '#9966cc'];
+
+for (let i = 0; i < 100; i++) {
+    const confettiPiece = document.createElement('div');
+    confettiPiece.className = 'confetti';
+    confettiPiece.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+    confettiPiece.style.left = `${Math.random() * 100}%`;
+    confettiPiece.style.animationDelay = `${Math.random()}s`;
+    confettiContainer.appendChild(confettiPiece);
+}
